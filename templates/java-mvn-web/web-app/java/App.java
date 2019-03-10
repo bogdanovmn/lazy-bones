0032dd3e9fedad7dcda800cc8b337bcac315ab8b
Mikhail N Bogdanov <bogdanovmn@gmail.com>
2019-03-10 15:33:07 +0300

    fix app startup

--- templates/java-mvn-web/web-app/java/App.java
+++ templates/java-mvn-web/web-app/java/App.java
@@ -6,10 +6,15 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "${pkgProjectPrefix}.web.orm")
 @EntityScan(basePackages = "${pkgProjectPrefix}.web.orm")
+@ComponentScan(basePackages = {
+	"${pkgProjectPrefix}.web.app",
+	"${pkgProjectPrefix}.web.orm.common"
+})
 @EnableTransactionManagement
 public class App {
 	public static void main(String[] args) {
---
+++ templates/java-mvn-web/web-app/java/infrastructure/config/TomcatConfig.java
@@ -0,0 +1,34 @@
+package ${pkgProjectPrefix}.web.app.infrastructure.config;
+
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TomcatConfig {
+	@Bean
+	public TomcatServletWebServerFactory tomcatEmbedded() {
+
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+
+		tomcat.addInitializers(servletContext -> servletContext.setSessionTimeout(5000));
+
+		tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+
+			// connector other settings...
+
+			// configure maxSwallowSize
+			if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+				// -1 means unlimited, accept bytes
+//				((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+				((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(100 * 1024*1024);
+			}
+
+		});
+
+		return tomcat;
+
+	}
+}
